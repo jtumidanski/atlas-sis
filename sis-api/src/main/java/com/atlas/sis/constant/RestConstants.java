974ea4fcf7da3914471357174d4b3049b570ccdf
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-01-07 11:40:38 -0500

    Add constant for accessing rest service.

---
+++ sis-api/src/main/java/com/atlas/sis/constant/RestConstants.java
@@ -0,0 +1,8 @@
+package com.atlas.sis.constant;
+
+public final class RestConstants {
+   private RestConstants() {
+   }
+
+   public static final String SERVICE = "sis";
+}
--- sis-base/src/main/java/com/atlas/sis/Server.java
+++ sis-base/src/main/java/com/atlas/sis/Server.java
@@ -3,12 +3,12 @@ package com.atlas.sis;
 import java.net.URI;
 
 import com.atlas.shared.rest.RestServerFactory;
-import com.atlas.shared.rest.RestService;
 import com.atlas.shared.rest.UriBuilder;
+import com.atlas.sis.constant.RestConstants;
 
 public class Server {
    public static void main(String[] args) {
-      URI uri = UriBuilder.host(RestService.SKILL_INFORMATION).uri();
+      URI uri = UriBuilder.host(RestConstants.SERVICE).uri();
       RestServerFactory.create(uri, "com.atlas.sis.rest");
    }
 }
