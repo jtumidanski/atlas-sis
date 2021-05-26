4a1fc41f4f7bb28b8b68b6b1634cb261c72cda34
Tumidanski Justin D <Justin.Tumidanski@dematic.com>
2021-05-26 08:09:10 -0400

    Generify the batch script.

--- docker-build.bat
+++ docker-build.bat
@@ -1,3 +1,4 @@
 @ECHO OFF
-IF "%1"=="NO-CACHE" docker build --no-cache --tag atlas-sis:latest .
-IF NOT "%1"=="NO-CACHE" docker build --tag atlas-sis:latest .
+FOR %%a IN ("%~dp0\.") DO SET "parent=%%~nxa"
+IF "%1"=="NO-CACHE" (docker build --no-cache -f Dockerfile --tag %parent%:latest .)
+IF NOT "%1"=="NO-CACHE" (docker build -f Dockerfile --tag %parent%:latest .)
